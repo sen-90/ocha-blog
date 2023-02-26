8d7269cd314a4a7e0d6250fad1e72dafdc8e1439
xxxx <cmuraoka21@gmail.com>
2023-02-26 13:00:28 +0900

    begin a project

---
+++ src/pages/index.tsx
@@ -0,0 +1,41 @@
+import { MicroCMSListResponse } from "microcms-js-sdk";
+import type { GetStaticProps, NextPage } from "next";
+import Link from "next/link";
+import { client } from "src/libs/client";
+
+export type Blog = {
+  title: string;
+  body: string;
+};
+
+type Props = MicroCMSListResponse<Blog>;
+
+const Home: NextPage<Props> = (props) => {
+  return (
+    <div>
+      <p className="text-gray-400">{`記事の総数: ${props.totalCount}件`}</p>
+      <ul className="mt-4 space-y-4">
+        {props.contents.map((content) => {
+          return (
+            <li key={content.id}>
+              <Link href={`/blog/${content.id}`}>
+                <a className="text-blue-800 text-xl underline hover:text-blue-400">{content.title}</a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export const getStaticProps: GetStaticProps<
+  MicroCMSListResponse<Blog>
+> = async () => {
+  const data = await client.getList<Blog>({ endpoint: "blog" });
+  return {
+    props: data,
+  };
+};
+
+export default Home;
